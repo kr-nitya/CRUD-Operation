44d1017658499a20989b19e3a311b83236091290
Nitya <nitya.m@knackroot.com>
2024-02-02 11:49:33 +0530

    Sequelize

--- controller/UserController.ts
+++ controller/UserController.ts
@@ -1,107 +1,122 @@
-import UserModel from "../model/UserModel";
 import { Request, Response } from "express";
 import { encryptPassword } from "../utility/encryption";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
 import { uploadToCloudinary } from "../utility/cloudinary";
-dotenv.config();
+import User from "../model/UserModel";
+import { Op } from "sequelize";
 
 export interface ExtendedRequest extends Request {
   decodedUsername?: string;
-  file?: Express.Multer.File; 
+  file?: Express.Multer.File;
 }
+
 class UserController {
-  static registerUser(req: Request, res: Response) {
+  static async registerUser(req: Request, res: Response) {
     const { name, address, phone, password, username } = req.body;
+
     if (!name || !address || !phone || !password || !username) {
       res.status(400).json({ error: "All Fields are required" });
       return;
     }
-    const encryptedPassword = encryptPassword(password);
-    UserModel.registerUser(
-      name,
-      address,
-      phone,
-      encryptedPassword,
-      username,
-      (err: any, result: any) => {
-        if (err) {
-          console.error("Error in Registration:", err);
-          // Check for duplicate entry error (ER_DUP_ENTRY)
-          if (err.code === "ER_DUP_ENTRY") {
-            if (err.message.includes("username")) {
-              return res.status(409).json({ error: "Username already exists" });
-            } else if (err.message.includes("phone")) {
-              return res
-                .status(409)
-                .json({ error: "Phone number already exists" });
-            }
-          }
 
-          res.status(500).json({ error: "Internal Server Error" });
-          return;
+    const encryptedPassword = encryptPassword(password);
+
+    try {
+      const user = await User.create({
+        name,
+        address,
+        phone,
+        password: encryptedPassword,
+        username,
+      });
+
+      res.json({
+        message: "Register successfully",
+        
+      });
+    } catch (error:any) {
+      console.error("Error in Registration:", error);
+
+      if (error.name === "SequelizeUniqueConstraintError") {
+        if (error.fields.includes("username")) {
+          return res.status(409).json({ error: "Username already exists" });
+        } else if (error.fields.includes("phone")) {
+          return res.status(409).json({ error: "Phone number already exists" });
         }
-        res.json({
-          message: "Register successfully",
-          insertedId: result.insertId,
-        });
       }
-    );
+
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-  static loginUser(req: Request, res: Response) {
+
+  static async loginUser(req: Request, res: Response) {
     const { username, password } = req.body;
+
     if (!password || !username) {
       res.status(400).json({ error: "All Fields are required" });
       return;
     }
+
     const encryptedPassword = encryptPassword(password);
-    UserModel.loginUser(username, encryptedPassword, (err, result) => {
-      if (err) {
-        console.error("Error in Login:", err);
-        res.status(500).json({ error: "Internal Server Error" });
-        return;
-      }
-      if (!result) {
+
+    try {
+      const user = await User.findOne({
+        where: {
+          [Op.or]: [{ username }, { phone: username }],
+        },
+      });
+
+      if (!user || user.getDataValue("password") !== encryptedPassword) {
         res.status(401).json({ error: "Invalid username/phone or password" });
         return;
       }
-      // Generate JWT token
+
       const secretKey = process.env.SECRET_KEY || "";
       const token = jwt.sign({ username }, secretKey, {
-        expiresIn: "1h", // Set the expiration time of the token
+        expiresIn: "1h",
       });
+
       res.json({
         message: "Login successful",
         token,
       });
-    });
+    } catch (error) {
+      console.error("Error in Login:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-  static getUserDetails(req: ExtendedRequest, res: Response) {
+
+  static async getUserDetails(req: ExtendedRequest, res: Response) {
     const { decodedUsername } = req;
 
     if (!decodedUsername) {
       return res.status(401).json({ error: "Unauthorized - Invalid token" });
     }
 
-    UserModel.getUserDetails(decodedUsername, (err, result) => {
-      if (err) {
-        console.error("Error fetching user details:", err);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
+    try {
+      const user = await User.findOne({
+        where: { username: decodedUsername },
+      });
 
-      if (!result || result.length === 0) {
+      if (!user) {
         return res.status(404).json({ error: "User not found" });
       }
+
       const userDetails = {
-        name: result[0].name,
-        address: result[0].address,
-        phone: result[0].phone,
-        username: result[0].username,
+        name: user.getDataValue("name"),
+        address: user.getDataValue("address"),
+        phone: user.getDataValue("phone"),
+        username: user.getDataValue("username"),
       };
+
       res.json({ message: "User details retrieved successfully", userDetails });
-    });
+    } catch (error) {
+      console.error("Error fetching user details:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-  static updateUserName(req: ExtendedRequest, res: Response) {
+
+  static async updateUserName(req: ExtendedRequest, res: Response) {
     const { decodedUsername } = req;
     const { name } = req.params;
 
@@ -109,69 +124,91 @@ class UserController {
       return res.status(401).json({ error: "Unauthorized - Invalid token" });
     }
 
-    UserModel.updateUserName(decodedUsername, name, (err, result) => {
-      if (err) {
-        console.error("Error updating user's name:", err);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
+    try {
+      const [_, updatedUser] = await User.update(
+        { name },
+        {
+          where: {
+            [Op.or]: [{ username: decodedUsername }, { phone: decodedUsername }],
+          },
+          returning: true,
+        }
+      );
 
-      if (result.affectedRows === 0) {
-        return res
-          .status(404)
-          .json({ error: "User not found or name not updated" });
+      if (!updatedUser || updatedUser.length === 0) {
+        return res.status(404).json({ error: "User not found or name not updated" });
       }
 
       res.json({ message: "User name updated successfully" });
-    });
+    } catch (error) {
+      console.error("Error updating user's name:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-  static updateAddress(req: ExtendedRequest, res: Response) {
+
+  static async updateAddress(req: ExtendedRequest, res: Response) {
     const { decodedUsername } = req;
     const { address } = req.params;
+
     if (!decodedUsername) {
       return res.status(401).json({ error: "Unauthorized - Invalid token" });
     }
-    UserModel.updateAddress(decodedUsername, address, (err, result) => {
-      if (err) {
-        console.error("Error updating user's address:", err);
-        return res.status(500).json({ error: "Internal Server Error" });
+
+    try {
+      const [_, updatedUser] = await User.update(
+        { address },
+        {
+          where: {
+            [Op.or]: [{ username: decodedUsername }, { phone: decodedUsername }],
+          },
+          returning: true,
+        }
+      );
+
+      if (!updatedUser || updatedUser.length === 0) {
+        return res.status(404).json({ error: "User not found or address not updated" });
       }
 
-      if (result.affectedRows === 0) {
-        return res
-          .status(404)
-          .json({ error: "User not found or address not updated" });
-      }
-
-      res.json({ message: "address updated successfully" });
-    });
+      res.json({ message: "Address updated successfully" });
+    } catch (error) {
+      console.error("Error updating user's address:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
   }
+
   static async uploadUserImage(req: ExtendedRequest, res: Response) {
     const { decodedUsername, file } = req;
-  
+
     if (!decodedUsername) {
       return res.status(401).json({ error: "Unauthorized - Invalid token" });
     }
-  
+
     if (!file) {
       return res.status(400).json({ error: "No image file provided" });
     }
-  
+
     try {
       // Upload image to Cloudinary
-      const result1 = await uploadToCloudinary(file.buffer);
-  
-      // Store image URL in the database
-      UserModel.updateUserProfileImage(decodedUsername, result1.secure_url, (err, result) => {
-        if (err) {
-          console.error("Error updating user's profile image:", err);
-          return res.status(500).json({ error: "Internal Server Error" });
-        }
-  
-        res.json({
-          message: "User image uploaded and URL stored successfully",
-          url: result1.secure_url,
-        });
+      const result = await uploadToCloudinary(file.buffer);
 
+      // Store image URL in the database
+      const [_, updatedUser] = await User.update(
+        { imageUrl: result.secure_url },
+        {
+          where: {
+            [Op.or]: [{ username: decodedUsername }, { phone: decodedUsername }],
+          },
+          returning: true,
+        }
+      );
+
+      if (!updatedUser || updatedUser.length === 0) {
+        return res.status(404).json({ error: "User not found or image URL not updated" });
+      }
+
+      res.json({
+        message: "User image uploaded and URL stored successfully",
+        url: result.secure_url,
       });
     } catch (error) {
       console.error("Error uploading image to Cloudinary:", error);
@@ -198,16 +235,23 @@ class UserController {
       );
   
       // Store file URLs in the database
-      UserModel.updateUserFileUrls(decodedUsername, fileUrls, (err, result) => {
-        if (err) {
-          console.error("Error updating user's file URLs:", err);
-          return res.status(500).json({ error: "Internal Server Error" });
+      const [_, updatedUser] = await User.update(
+        { fileUrls },
+        {
+          where: {
+            [Op.or]: [{ username: decodedUsername }, { phone: decodedUsername }],
+          },
+          returning: true,
         }
+      );
   
-        res.json({
-          message: "Files uploaded and URLs stored successfully",
-          fileUrls,
-        });
+      if (!updatedUser || updatedUser.length === 0) {
+        return res.status(404).json({ error: "User not found or file URLs not updated" });
+      }
+  
+      res.json({
+        message: "Files uploaded and URLs stored successfully",
+        fileUrls,
       });
     } catch (error) {
       console.error("Error uploading files to Cloudinary:", error);
--- model/UserModel.ts
+++ model/UserModel.ts
@@ -1,82 +1,49 @@
-import * as mysql from "mysql";
-import connection from "./connection";
-class UserModel {
-  static registerUser(
-    name: string,
-    address: string,
-    phone: string,
-    password: string,
-    username: string,
-    callback: mysql.queryCallback
-  ) {
-    const sql: string = "INSERT INTO users values (?,?,?,?,?)";
-    connection.query(sql, [name, address, phone, password, username], callback);
+import { Sequelize, DataTypes, Dialect } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DIALECT as Dialect,
   }
+);
 
-  static loginUser(
-    username: string,
-    password: string,
-    callback: mysql.queryCallback
-  ) {
-    const sql = "SELECT * FROM users WHERE (username=? OR phone=?)";
-    connection.query(sql, [username, username], (err, results) => {
-      if (err) {
-        callback(err, null);
-        return;
-      }
-
-      if (results.length === 0) {
-        // No user found with the given username/phone
-        callback(null, null);
-        return;
-      }
-      // Check if the provided password matches the stored encrypted password
-      const storedPassword: string = results[0].password;
-      if (password === storedPassword) {
-        callback(null, results);
-      } else {
-        callback(null, null); // Passwords do not match
-      }
-    });
+const User = sequelize.define(
+  "User",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+    },
+    fileUrls: {
+      type: DataTypes.JSON,
+    },
+  },
+  {
+    tableName: "usersequelize",
   }
-  static getUserDetails(username: string, callback: mysql.queryCallback) {
-    const sql = "SELECT * FROM users WHERE username=?";
-    connection.query(sql, [username], callback);
-  }
-  static updateUserName(
-    username: string,
-    newName: string,
-    callback: mysql.queryCallback
-  ) {
-    const sql = "UPDATE users SET name=? WHERE (username=? OR phone=?)";
-    connection.query(sql, [newName, username, username], callback);
-  }
-  static updateAddress(
-    username: string,
-    address: string,
-    callback: mysql.queryCallback
-  ) {
-    const sql = "UPDATE users SET address=? WHERE (username=? OR phone=?)";
-    connection.query(sql, [address, username, username], callback);
-  }
-  static updateUserProfileImage(
-    username: string,
-    imageUrl: string,
-    callback: mysql.queryCallback
-  ) {
-   
-    const sql = "UPDATE users SET imageUrl=? WHERE (username=? OR phone=?)";
-    connection.query(sql, [imageUrl, username, username], callback);
-  }
-  static updateUserFileUrls(
-    username: string,
-    fileUrls: string[],
-    callback: mysql.queryCallback
-  ) {
-    const sql = "UPDATE users SET fileUrls=? WHERE (username=? OR phone=?)";
-    connection.query(sql, [JSON.stringify(fileUrls), username, username], callback);
-  }
-  
-}
-
-export default UserModel;
+);
+export default User;
